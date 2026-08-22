ec99eb66609299c0445c8ba56656dd3fe4c0ad13
Llennpie <44985633+Llennpie@users.noreply.github.com>
2026-08-22 18:23:29 -0400

    Bump version -> 1.2

--- src/saturn/saturn_version.h
+++ src/saturn/saturn_version.h
@@ -2,11 +2,11 @@
 #define DEVSTAGE_RELEASE 1
 
 // comment this out for release builds
-#define VERSION_PRERELEASE
+//#define VERSION_PRERELEASE
 
 #define VERSION_STAGE DEVSTAGE_RELEASE
-#define VERSION_MAJOR 1
-#define VERSION_MINOR 1
+#define VERSION_MAJOR 2
+#define VERSION_MINOR 0
 #define VERSION_PATCH 0
 
 #define _STR(x) #x
